e56eea58ae5edd5fed2f289d06692f1c0306e987
Abdallah <abdallahchaar90@gmail.comh>
2021-01-15 11:07:29 +0100

    ett exampel om strings methods som är link och hur vi kan ligga link

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 /*global console, alert, prompt*/
 
+// trim : för att ta bort tomrum
 
+/*
 
+var myString ="     Hello Iam Abdallah     ";
+
+console.log(myString);
+
+var myNewString = myString.trim();
+
+console.log(myNewString);
+*/
+
+var mystring = "Google";
+
+console.log("Go To Google By This Link => " + mystring.link("http://www.google.com"))
